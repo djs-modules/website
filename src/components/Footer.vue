f6bc1793b8d298f34d6ce3e26d5bba23303c8f81
bad-boy-discord <andreyzero123@gmail.com>
2022-06-21 21:01:26 +0300

    :rocket: Website
    The site is currently down because not all modules have been released to the public. As soon as all modules will be prepared and released - the site will work.

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+	<footer class="bg-discord-blurple-560">
+		<div class="max-w-3xl mx-auto text-center px-16 pt-10 pb-4 text-gray-200">
+			<strong><router-link to="/">djs-modules</router-link></strong>
+			<p class="mb-4">A set of modules that you can use in your Discord bot.</p>
+		</div>
+	</footer>
+</template>
---
+++ src/data/NotifierSource.ts
@@ -0,0 +1,13 @@
+import semver from 'semver';
+
+import DocsSource from './DocsSource';
+
+const branchBlacklist = new Set(['docs']);
+export default new DocsSource({
+	id: 'notifier',
+	name: 'Notifier',
+	repo: 'djs-modules/notifier',
+	defaultTag: 'stable',
+	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
+	tagFilter: (tag: string) => semver.gte(tag, '9.0.0'),
+});
